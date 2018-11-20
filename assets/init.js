e0852bcf5b96b95a7677a4c81e8cd828064ca161
AHMAD WAEL <dev.ahmedwael@gmail.com>
2018-11-21 01:16:42 +0200

    First Release

---
+++ assets/init.js
@@ -0,0 +1,23 @@
+(function ($) {
+    "use strict";
+    /**
+     * Show / Hide Search box in navbar
+     */
+    (function () {
+        var open_btn = $('.bb_open_showcase');
+        open_btn.on('click', function (e) {
+            e.preventDefault();
+            $('.bb_theme_demos_container').addClass('visible transperancy');
+            $('body').addClass('bb-ovh');
+            $('.bb_theme_demos_floating_buttons').hide();
+        });
+
+        var close_button = $('.bb_theme_demos_close a');
+        close_button.on('click', function (e) {
+            e.preventDefault();
+            $('.bb_theme_demos_container').removeClass('visible transperancy');
+            $('body').removeClass('bb-ovh');
+            $('.bb_theme_demos_floating_buttons').show();
+        });
+    })();
+})(jQuery);
